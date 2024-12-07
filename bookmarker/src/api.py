1973724cdd5959d499e89d86f09b7aab26d4d010
Maor <maortz@gmail.com>
2024-12-07 23:19:34 +0200

    Support Tanah Yomi by hebrew year

--- bookmarker/src/api.py
+++ bookmarker/src/api.py
@@ -7,7 +7,7 @@ from typing import Optional
 
 from fastapi import FastAPI, File, HTTPException, Query, UploadFile
 from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
-
+from pyluach.dates import HebrewDate
 from src.config import Args
 from src.core import run_from_lst, run_from_str
 from src.input_generator import convert_date, generate_csv, learning_days
@@ -29,20 +29,43 @@ async def health_check():
     return {"status": "healthy"}
 
 
+def heb_to_int(ch: str) -> int:
+    if ord(ch) >= ord("ק"):
+        return (ord(ch) - ord("ק") + 1) * 100
+    if ord(ch) == ord("צ"):
+        return 90
+    if ord(ch) == ord("פ"):
+        return 80
+    if ord(ch) in range(ord("נ"), ord("ע") + 1):
+        return (ord(ch) - ord("כ") - 2) * 10
+    if ord(ch) == ord("מ"):
+        return 40
+    if ord(ch) in (ord("כ"), ord("ל")):
+        return (ord(ch) - 11) * 10
+    return ord(ch) - ord("א") + 1
+
+
+def get_heb_year(year: str) -> int:
+    if not isinstance(year, str) or len(year) > 5 or not year:
+        raise
+    thousands = 1000 * heb_to_int(year[0])
+    return thousands + sum(map(heb_to_int, year[1:]))
+
+
+def get_simhat_tora_by(year: str) -> tuple[HebrewDate, HebrewDate]:
+    s = HebrewDate(get_heb_year(year), 7, 23)
+    return s, s.add(years=1).subtract(days=1)
+
+
 @app.get("/bookmarker/tanah")
 async def gen_tanah_htmlpage(
     width: int = Query(10, description="Bookmark width (cm)"),
     height: int = Query(15, description="Bookmark height (cm)"),
     font: int = Query(12, description="Font size"),
-    start_date: datetime.date = Query(
+    year: str = Query(
         ...,
-        description="Start date (in the format of 2024-10-03)",
-        examples=["2024-10-03"],
-    ),
-    end_date: Optional[datetime.date] = Query(
-        None,
-        description="End date, inclusive (default to 1 hebrew year)",
-        examples=[None, "2025-09-22"],
+        description="Hebrew year (in the format of התשפה)",
+        examples=["התשפה"],
     ),
     shabbos: bool = Query(True, description="Do not schedule learning on Shabbos"),
     major_holidays: bool = Query(
@@ -59,7 +82,7 @@ async def gen_tanah_htmlpage(
     bold: bool = Query(True, description="Bold Shabbos or any non-learning day"),
 ):
     days = learning_days(
-        *convert_date(start_date, end_date),
+        *get_simhat_tora_by(year),
         shabbos=shabbos,
         major_holidays=major_holidays,
         minor_holidays=minor_holidays,
@@ -74,7 +97,7 @@ async def gen_tanah_htmlpage(
     csv_decoded = Path(f"examples/tanah_yomi_{days}.csv").read_text(encoding="utf-8")
     lines = csv_decoded.splitlines()
     input_lines = generate_csv(
-        *convert_date(start_date, end_date),
+        *get_simhat_tora_by(year),
         iter(lines),
         shabbos=shabbos,
         major_holidays=major_holidays,
